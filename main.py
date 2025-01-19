14ec0f0cf4784adc6b53e4eb8e0c59c65b1af3f4
AntonisK <101641999+AntonisKatsifas@users.noreply.github.com>
2025-01-19 22:01:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,23 @@ from search_engine import search_interface
 from evaluation import evaluate_system
 
 def main():
-    # # Step 1ο
-    # queries = "Deep learning,Document classification,Information retrieval,Natural language processing,Learning to rank,Information retrieval,Recommender system,Large language model,Prompt engineering,Machine learning,Recurrent neural network,Ranking Algorithms,Climate Change,History of Art,Health and Nutrition,The History of the Olympic Games,Cave exploration".split(",")
-    # fetch_and_save_articles(queries)
+    # Step 1ο
+    queries = "Deep learning,Document classification,Information retrieval,Natural language processing,Learning to rank,Information retrieval,Recommender system,Large language model,Prompt engineering,Machine learning,Recurrent neural network,Ranking Algorithms,Climate Change,History of Art,Health and Nutrition,The History of the Olympic Games,Cave exploration".split(",")
+    fetch_and_save_articles(queries)
     
-    # # Step 2ο
-    # process_articles("wikipedia_articles.json", "processed_wikipedia_articles.json")
+    # Step 2ο
+    process_articles("wikipedia_articles.json", "processed_wikipedia_articles.json")
 
-    # # Step 3ο
-    # input_filename = "processed_wikipedia_articles.json"
-    # output_filename = "inverted_index.json"
-    # articles = load_processed_articles(input_filename)
-    # save_inverted_index(create_inverted_index(articles), output_filename)
+    # Step 3ο
+    input_filename = "processed_wikipedia_articles.json"
+    output_filename = "inverted_index.json"
+    articles = load_processed_articles(input_filename)
+    save_inverted_index(create_inverted_index(articles), output_filename)
 
-    # # Step 4ο
-    # search_interface()
+    # Step 4ο
+    search_interface()
 
-    # # Step 5ο
-    
+    # Step 5ο
     evaluate_system()
 
 if __name__ == "__main__":
